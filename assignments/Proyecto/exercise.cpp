0c0bf95e40d6a19a04539dc4b05eba0047ef2940
Damian Reza <112132569+DamianRo1@users.noreply.github.com>
2023-11-30 23:33:18 -0600

    Update exercise.cpp

--- assignments/Proyecto/exercise.cpp
+++ assignments/Proyecto/exercise.cpp
@@ -1,102 +1,187 @@
 #include <iostream>
+#include <fstream>
 #include <iomanip>
+#include "Aeropuerto.hpp"
+#include "Vuelo.hpp"
 
-class Hora {
-private:
-    int horas;
-    int minutos;
-
-public:
-    Hora() : horas(0), minutos(0) {}
-
-    Hora(int horas, int minutos) : horas(horas), minutos(minutos) {}
-
-    int getHoras() const {
-        return horas;
-    }
-
-    int getMinutos() const {
-        return minutos;
-    }
-
-    void imprime() const {
-        std::cout << std::setfill('0') << std::setw(2) << horas << ":" << std::setw(2) << minutos;
-    }
-};
-
-class Aeropuerto {
-private:
-    std::string codigo;
-    std::string ciudad;
-    std::string pais;
-
-public:
-    Aeropuerto(const std::string& codigo, const std::string& ciudad, const std::string& pais)
-        : codigo(codigo), ciudad(ciudad), pais(pais) {}
-
-    void imprime() const {
-        std::cout << "Aeropuerto: " << codigo << " - " << ciudad << ", " << pais;
-    }
-};
-
-class Vuelo {
-private:
-    std::string ID;
-    Aeropuerto destino;
-    std::string aerolinea;
-    int numPas;
-    Hora salidaEstim;
-    Hora salidaReal;
-    int estatus;
-
-public:
-    Vuelo() : salidaReal(0, 0), estatus(1), destino("", "", "") {}
-
-    Vuelo(const std::string& ID, const Aeropuerto& destino, const std::string& aerolinea, int numPas, const Hora& salidaEstim)
-        : ID(ID), destino(destino), aerolinea(aerolinea), numPas(numPas), salidaEstim(salidaEstim), salidaReal(0, 0), estatus(1) {}
-
-    std::string getID() const { return ID; }
-    Aeropuerto getDestino() const { return destino; }
-    std::string getAerolinea() const { return aerolinea; }
-    int getNumPas() const { return numPas; }
-    Hora getSalidaEstim() const { return salidaEstim; }
-    Hora getSalidaReal() const { return salidaReal; }
-    int getEstatus() const { return estatus; }
-
-    void setId(const std::string& newID) { ID = newID; }
-    void setDestino(const Aeropuerto& newDestino) { destino = newDestino; }
-    void setAerolinea(const std::string& newAerolinea) { aerolinea = newAerolinea; }
-    void setNumPas(int newNumPas) { numPas = newNumPas; }
-    void setEstatus(int newEstatus) { estatus = newEstatus; }
-    void setSalidaEstim(const Hora& newSalidaEstim) { salidaEstim = newSalidaEstim; }
-    void setSalidaReal(const Hora& newSalidaReal) { salidaReal = newSalidaReal; }
-
-    int calculaTiempoRetraso() const {
-        return (salidaReal.getHoras() * 60 + salidaReal.getMinutos()) - (salidaEstim.getHoras() * 60 + salidaEstim.getMinutos());
-    }
-
-    void imprime() const {
-        std::cout << "ID: " << ID << std::endl;
-        destino.imprime();
-        std::cout << "\nAerolínea: " << aerolinea << "\nNúmero de pasajeros: " << numPas
-                  << "\nSalida estimada: ";
-        salidaEstim.imprime();
-        std::cout << "\nSalida real: ";
-        salidaReal.imprime();
-        std::cout << "\nEstatus: " << (estatus == 1 ? "A tiempo" : "Demorado") << std::endl;
-    }
-};
+const int MAX_AEROPUERTOS = 7; 
+const int MAX_VUELOS = 20;    
 
 int main() {
-    // Ejemplo de uso
-    Aeropuerto destino("JFK", "Nueva York", "EEUU");
-    Hora salidaEstimada(12, 30);
+    Aeropuerto aeropuertos[MAX_AEROPUERTOS];
+    Vuelo vuelos[MAX_VUELOS];
 
-    Vuelo vuelo1("ABC123", destino, "Delta", 150, salidaEstimada);
-    vuelo1.setSalidaReal(Hora(13, 0));
-    vuelo1.setEstatus(0);
+    std::ifstream file("aeropuertos.txt");
+    if (file.is_open()) {
+        for (int i = 0; i < MAX_AEROPUERTOS; ++i) {
+            std::string clave, nombre, ciudad, pais;
+            file >> clave >> nombre >> ciudad >> pais;
+            aeropuertos[i] = Aeropuerto(clave, nombre, ciudad, pais);
+        }
+        file.close();
+    } else {
+        std::cout << "Error\n";
+    }
+    std::ifstream file("datosVuelos.txt");
+    if (file.is_open()) {
+        for (int i = 0; i < MAX_VUELOS; ++i) {
+            std::string id, destinoClave, aerolinea;
+            int numPasajeros, salidaEstimHora, salidaEstimMin;
+            file >> id >> destinoClave >> aerolinea >> numPasajeros >> salidaEstimHora >> salidaEstimMin;
 
-    vuelo1.imprime();
+            Aeropuerto destino;
+            int pos = buscaAeropuerto(destinoClave, aeropuertos);
+            if (pos != -1) {
+                destino = aeropuertos[pos];
+            } else {
+                std::cout << "Error" << id << "\n";
+                continue;
+            }
+
+            Hora salidaEstim(salidaEstimHora, salidaEstimMin);
+            vuelos[i] = Vuelo(id, destino, aerolinea, numPasajeros, salidaEstim);
+        }
+        file.close();
+    } else {
+        std::cout << "Error '\n";
+    }
+
+    int opcion;
+
+    do {
+        std::cout << "\nMenu:\n";
+        std::cout << "1. Desplegar la lista de Aeropuertos destino\n";
+        std::cout << "2. Desplegar la lista de Vuelos\n";
+        std::cout << "3. Desplegar Vuelos que van a un determinado destino\n";
+        std::cout << "4. Desplegar Vuelos que salen en un rango de tiempo y 'A tiempo'\n";
+        std::cout << "5. Capturar la hora de salida real de un Vuelo\n";
+        std::cout << "6. Salir\n";
+        std::cout << "Ingrese su opcion: ";
+        std::cin >> opcion;
+
+        switch (opcion) {
+            case 1:
+                desplegarAeropuertos(aeropuertos);
+                break;
+            case 2:
+                desplegarVuelos(vuelos);
+                break;
+            case 3:
+                desplegarVuelosDestino(vuelos, aeropuertos);
+                break;
+            case 4:
+                desplegarVuelosEnRango(vuelos);
+                break;
+            case 5:
+                capturarHoraSalidaReal(vuelos, aeropuertos);
+                break;
+            case 6:
+                std::cout << "Saliendo del programa.\n";
+                break;
+            default:
+                std::cout << "Opcion invalida. Intente de nuevo.\n";
+        }
+
+    } while (opcion != 6);
 
     return 0;
 }
+
+
+int buscaAeropuerto(const std::string& clave, Aeropuerto aeropuertos[MAX_AEROPUERTOS]) {
+    for (int i = 0; i < MAX_AEROPUERTOS; ++i) {
+        if (aeropuertos[i].getClave() == clave) {
+            return i; 
+        }
+    }
+    return -1; 
+}
+
+void desplegarAeropuertos(const Aeropuerto aeropuertos[MAX_AEROPUERTOS]) {
+    std::cout << "Aeropuertos:\n";
+    for (int i = 0; i < MAX_AEROPUERTOS; ++i) {
+        aeropuertos[i].imprime();
+    }
+}
+
+void desplegarVuelos(const Vuelo vuelos[MAX_VUELOS]) {
+    std::cout << "Vuelos:\n";
+    for (int i = 0; i < MAX_VUELOS; ++i) {
+        vuelos[i].imprime();
+    }
+}
+
+void desplegarVuelosDestino(const Vuelo vuelos[MAX_VUELOS], const Aeropuerto aeropuertos[MAX_AEROPUERTOS]) {
+    std::string clave;
+    std::cout << "Ingrese la clave del aeropuerto destino: ";
+    std::cin >> clave;
+
+    int pos = buscaAeropuerto(clave, aeropuertos);
+    if (pos != -1) {
+        std::cout << "Vuelos con destino a " << aeropuertos[pos].getNombre() << ":\n";
+        for (int i = 0; i < MAX_VUELOS; ++i) {
+            if (vuelos[i].getDestino().getClave() == clave) {
+                vuelos[i].imprime();
+            }
+        }
+    } else {
+        std::cout << "Error: Aeropuerto con clave " << clave << " no encontrado\n";
+    }
+}
+
+void desplegarVuelosEnRango(const Vuelo vuelos[MAX_VUELOS]) {
+    int horaInicio, minInicio, horaFin, minFin;
+
+    std::cout << "Ingrese la hora de inicio (formato 24 horas - hh:mm): ";
+    std::cin >> horaInicio >> minInicio;
+
+    std::cout << "Ingrese la hora de fin (formato 24 horas - hh:mm): ";
+    std::cin >> horaFin >> minFin;
+
+    Hora inicio(horaInicio, minInicio);
+    Hora fin(horaFin, minFin);
+
+    std::cout << "Vuelos en el rango de tiempo " << inicio.getHora() << ":" << inicio.getMin() << " a " << fin.getHora() << ":" << fin.getMin() << ":\n";
+
+    for (int i = 0; i < MAX_VUELOS; ++i) {
+        if (vuelos[i].getSalidaEstim() >= inicio && vuelos[i].getSalidaEstim() <= fin) {
+            vuelos[i].imprime();
+        }
+    }
+}
+
+void capturarHoraSalidaReal(Vuelo vuelos[MAX_VUELOS], const Aeropuerto aeropuertos[MAX_AEROPUERTOS]) {
+    std::string id;
+    std::cout << "Ingrese el ID del vuelo: ";
+    std::cin >> id;
+
+    int pos = -1;
+    for (int i = 0; i < MAX_VUELOS; ++i) {
+        if (vuelos[i].getId() == id) {
+            pos = i;
+            break;
+        }
+    }
+
+    if (pos != -1) {
+        int horaReal, minReal;
+        std::cout << "Ingrese la hora de salida real (formato 24 horas - hh:mm): ";
+        std::cin >> horaReal >> minReal;
+
+        Hora salidaReal(horaReal, minReal);
+
+        vuelos[pos].setSalidaReal(salidaReal);
+
+        if (vuelos[pos].getSalidaReal() > vuelos[pos].getSalidaEstim()) {
+            vuelos[pos].setEstatus(0); 
+            int minutosRetraso = vuelos[pos].calculaTiempoRetraso();
+            std::cout << "El vuelo está demorado " << minutosRetraso << " minutos.\n";
+        } else {
+            vuelos[pos].setEstatus(1); 
+            std::cout << "El vuelo está a tiempo.\n";
+        }
+    } else {
+        std::cout << "Error: Vuelo con ID " << id << " no encontrado\n";
+    }
+}
+
